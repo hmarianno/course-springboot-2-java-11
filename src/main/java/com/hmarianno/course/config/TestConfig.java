b6ddc9bcbded0a2a715143b87ff1924639a168b0
hmarianno <hmarianno@gmail.com>
2021-05-12 18:51:54 -0300

    Payment, one-to-one association

--- src/main/java/com/hmarianno/course/config/TestConfig.java
+++ src/main/java/com/hmarianno/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hmarianno.course.entities.Category;
 import com.hmarianno.course.entities.Order;
 import com.hmarianno.course.entities.OrderItem;
+import com.hmarianno.course.entities.Payment;
 import com.hmarianno.course.entities.Product;
 import com.hmarianno.course.entities.User;
 import com.hmarianno.course.entities.enums.OrderStatus;
@@ -81,7 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());	
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
---
+++ src/main/java/com/hmarianno/course/entities/Payment.java
@@ -0,0 +1,63 @@
+package com.hmarianno.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private Instant moment;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {}
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		Id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+}
